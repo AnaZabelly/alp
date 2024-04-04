bb9fed61f1dac2083d0fad3c8787d95dbe3f48d5
AnaZabelly <164894874+AnaZabelly@users.noreply.github.com>
2024-04-04 08:59:21 -0300

    Add files via upload

---
+++ ex03.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt('digite um número'))
+if (numero % 5 == 0) {
+    document.write("o número é divisível por 5")
+} else {
+    document.write("o número não é divisivel por 5")
+}
